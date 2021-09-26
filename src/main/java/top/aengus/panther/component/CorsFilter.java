fd739ee7212bcd6b1ef09da4d1e5794b8e0c8430
Aengus Sun <sys6511@126.com>
2021-09-26 14:30:57 +0800

    refactor: Cors请求允许请求头与方法魔数抽取

--- src/main/java/top/aengus/panther/component/CorsFilter.java
+++ src/main/java/top/aengus/panther/component/CorsFilter.java
@@ -1,6 +1,8 @@
 package top.aengus.panther.component;
 
 import org.springframework.stereotype.Component;
+import top.aengus.panther.core.Constants;
+import top.aengus.panther.tool.StringUtil;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -10,13 +12,20 @@ import java.io.IOException;
 @Component
 public class CorsFilter implements Filter {
 
+    private static final String[] ALLOW_METHODS = {"GET", "POST", "PUT", "DELETE" };
+
+    private static final String[] ALLOW_HEADERS = {
+            "Content-Type", "X-CAF-Authorization-Token", "X-TOKEN", "Set-Cookie",
+            Constants.AUTHORIZATION, Constants.UPLOAD_TOKEN
+    };
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse res = (HttpServletResponse) response;
         res.addHeader("Access-Control-Allow-Credentials", "true");
         res.addHeader("Access-Control-Allow-Origin", "*");
-        res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-        res.addHeader("Access-Control-Allow-Headers", "Content-Type,X-CAF-Authorization-Token,sessionToken,X-TOKEN,Authorization,Upload-Token,Set-Cookie");
+        res.addHeader("Access-Control-Allow-Methods", StringUtil.joinToString(ALLOW_METHODS));
+        res.addHeader("Access-Control-Allow-Headers", StringUtil.joinToString(ALLOW_HEADERS));
         if (((HttpServletRequest) request).getMethod().equals("OPTIONS")) {
             response.getWriter().println("ok");
             return;
--- src/main/java/top/aengus/panther/tool/StringUtil.java
+++ src/main/java/top/aengus/panther/tool/StringUtil.java
@@ -1,5 +1,7 @@
 package top.aengus.panther.tool;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * @author Aengus Sun (sys6511@126.com)
  * <p>
@@ -14,4 +16,16 @@ public class StringUtil {
     public static boolean isNotEmpty(String string) {
         return !isEmpty(string);
     }
+
+    public static String joinToString(String[] strings) {
+        return joinToString(strings, ",");
+    }
+
+    public static String joinToString(String[] strings, String separator) {
+        if (strings == null || strings.length == 0) {
+            return "";
+        }
+        return StringUtils.join(strings, separator);
+    }
+
 }
