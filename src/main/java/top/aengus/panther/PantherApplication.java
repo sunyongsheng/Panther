f2461106e3c658df82c972838ec14568179022a5
Aengus Sun <sys6511@126.com>
2021-10-02 17:11:51 +0800

    feat: add async support

--- src/main/java/top/aengus/panther/PantherApplication.java
+++ src/main/java/top/aengus/panther/PantherApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EntityScan(basePackages = "top.aengus.panther.model")
+@EnableAsync
 @SpringBootApplication
 public class PantherApplication {
 
