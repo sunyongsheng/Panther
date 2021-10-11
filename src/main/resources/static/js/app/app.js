ef469b14ef4c5f508c0a40211c3563b20e2fbe66
Aengus Sun <sys6511@126.com>
2021-10-12 00:52:26 +0800

    feat: AppToken生成功能

--- src/main/resources/static/js/app/app.js
+++ src/main/resources/static/js/app/app.js
@@ -2,6 +2,14 @@ async function getAllAppData(page, pageSize) {
     return axiosClient.get(`/api/v1/apps?page=${page}&page_size=${pageSize}`)
 }
 
+async function hasGeneratedToken(appKey) {
+    return axiosClient.get(`api/v1/app/uploadToken?app_key=${appKey}`)
+}
+
+async function generateUploadToken(appKey) {
+    return axiosClient.post(`api/v1/app/uploadToken?app_key=${appKey}`)
+}
+
 async function updateAppInfo(appKey, param) {
     return axiosClient.put(`/api/v1/app/?app_key=${appKey}`, param)
 }
