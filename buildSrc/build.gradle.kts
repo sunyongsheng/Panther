05fd8339faaa630641a20b0606da6bc29a1ffa4b
Aengus Sun <sys6511@126.com>
2021-11-11 15:44:43 +0800

    feat: 修改构建脚本

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,23 +1,7 @@
-apply(plugin = "java-gradle-plugin")
-apply(plugin = "kotlin")
-
-val kotlinVersion = "1.5.30"
-
-buildscript {
-
-    repositories {
-        mavenCentral()
-    }
-
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
-    }
+plugins {
+    `kotlin-dsl`
 }
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
 }
