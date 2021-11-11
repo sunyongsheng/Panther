92f3f6eacab1232b9a106b0f1fefdcf16996ad49
Aengus Sun <sys6511@126.com>
2021-11-11 11:06:49 +0800

    fix: API返回值数量超出

--- src/main/java/top/aengus/panther/controller/api/AdminController.java
+++ src/main/java/top/aengus/panther/controller/api/AdminController.java
@@ -60,7 +60,7 @@ public class AdminController extends ApiV1Controller {
             if (i > 0) return -1;
             else if (i < 0) return 1;
             else return 0;
-        }).collect(Collectors.toList()));
+        }).limit(7).collect(Collectors.toList()));
     }
 
     @PutMapping("/admin/password")
